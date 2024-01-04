2eb8447c10332a5c2662b61a6708f6a215028527
AllwinFranklin <allwinanto360@gmail.com>
2024-01-04 13:19:53 +0530

    Added Entity (Model) to Book.Domain project

---
+++ Book/Book.Domain/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿using Book.Domain.Common;
+
+namespace Book.Domain.Entities
+{
+    public class Book : EntityBase
+    {
+        public string Publisher { get; set; }
+        public string Title { get; set; }
+        public string AuthorLastName { get; set; }
+        public string AuthorFirstName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
